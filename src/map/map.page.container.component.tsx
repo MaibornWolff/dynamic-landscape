9fe845d8a01bb4b9bc54b67f0ff87b413e01fa94
Elias Keis <elias.keis@maibornwolff.de>
2020-04-30 12:58:05 +0200

    fix format
    as suggested by @cstanger

--- src/map/map.page.container.component.tsx
+++ src/map/map.page.container.component.tsx
@@ -1,36 +1,27 @@
-import { connect } from 'react-redux';
-import { Dispatch } from 'redux';
-import { IState } from '../reducers';
-import {
-  getLoadingStatus,
-  getDetailService,
-  getContent, getUnfilteredContent
-} from './selectors/map.selector';
-import {
-  setContent,
-  setDetailService,
-  deleteDetailService
-} from './actions/map.actions';
+import {connect} from 'react-redux';
+import {Dispatch} from 'redux';
+import {IState} from '../reducers';
+import {getContent, getDetailService, getLoadingStatus, getUnfilteredContent} from './selectors/map.selector';
+import {deleteDetailService, setContent, setDetailService} from './actions/map.actions';
 import MapComponant from './map.page.component';
 
-import { DemoData } from '../assets/data/dataType';
+import {DemoData} from '../assets/data/dataType';
 
 
 const mapStateToProps = (state: IState) => ({
-    laoding: getLoadingStatus(state.Map),
-    content: getContent(state.Map),
-    unfilteredContent: getUnfilteredContent(state.Map),
-    detailService: getDetailService(state.Map)
-  });
-  
-  const mapDispatchToProps = (dispatch: Dispatch) => ({
-    setContent: (content: Array<DemoData>) => dispatch(setContent(content)),
-    setDetailService: (service: DemoData) => dispatch(setDetailService(service)),
-    deleteDetailService: () => dispatch(deleteDetailService())
-  });
-  
-  export const Map = connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )(MapComponant);
-  
+  laoding: getLoadingStatus(state.Map),
+  content: getContent(state.Map),
+  unfilteredContent: getUnfilteredContent(state.Map),
+  detailService: getDetailService(state.Map)
+});
+
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  setContent: (content: Array<DemoData>) => dispatch(setContent(content)),
+  setDetailService: (service: DemoData) => dispatch(setDetailService(service)),
+  deleteDetailService: () => dispatch(deleteDetailService())
+});
+
+export const Map = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MapComponant);
