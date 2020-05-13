30a352007dbeee85ca110415c252511027cdcfd1
Elias Keis <elias.keis@maibornwolff.de>
2020-05-13 10:24:31 +0200

    extract service button component

--- src/map/components/landscape/landscape.component.tsx
+++ src/map/components/landscape/landscape.component.tsx
@@ -5,10 +5,9 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import {IconButton, Tooltip, Typography} from '@material-ui/core';
+import {Typography} from '@material-ui/core';
 import {DemoData, Providers} from '../../../assets/data/dataType';
-import LazyLoad from 'react-lazyload';
-import classNames from 'classnames';
+import ServiceButton from './servicebutton.component';
 
 interface Props {
   setDetailService: (service: DemoData) => void;
@@ -22,16 +21,6 @@ const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     table: {
       minWidth: 100,
-    },
-    serviceButton: {
-      padding: 3,
-    },
-    serviceIcon: {
-      height: 25,
-      width: 25,
-    },
-    filteredService: {
-      opacity: 0.15,
     },
     header: {
       backgroundColor: theme.palette.primary.main,
@@ -68,33 +57,6 @@ export default function Landscape(props: Props) {
   const isServiceFiltered = (service: DemoData): boolean =>
     !props.filteredContent.includes(service);
 
-  const renderService = (
-    service: DemoData,
-    index: number,
-    isFiltered: boolean,
-    isFirstUnfiltered: boolean
-  ) => (
-    <Tooltip key={index} title={service.service}>
-      <IconButton
-        aria-label={service.service}
-        onClick={() => setDetailService(service)}
-        className={classes.serviceButton}
-        ref={isFirstUnfiltered ? firstUnfilteredService : undefined}
-      >
-        <LazyLoad height={25}>
-          <img
-            src={service.img}
-            alt={service.service}
-            className={classNames(
-              classes.serviceIcon,
-              isFiltered && classes.filteredService
-            )}
-          />
-        </LazyLoad>
-      </IconButton>
-    </Tooltip>
-  );
-
   const renderCategories = () => {
     let noUnfilteredServiceYet = true;
     return categories.map((category, i) => (
@@ -109,11 +71,16 @@ export default function Landscape(props: Props) {
                 const isFiltered = isServiceFiltered(service);
                 const isFirstUnfiltered = noUnfilteredServiceYet && !isFiltered;
                 if (isFirstUnfiltered) noUnfilteredServiceYet = false;
-                return renderService(
-                  service,
-                  index,
-                  isFiltered,
-                  isFirstUnfiltered
+                return (
+                  <ServiceButton
+                    key={index}
+                    service={service}
+                    setDetailService={setDetailService}
+                    isFiltered={isFiltered}
+                    buttonRef={
+                      isFirstUnfiltered ? firstUnfilteredService : undefined
+                    }
+                  />
                 );
               }
             )}
