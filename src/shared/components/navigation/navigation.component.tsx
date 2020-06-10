ed3666a36e9162665fe1b277102e4a6bfeefc3ea
cstanger <ch.stanger@me.com>
2020-06-10 09:28:27 +0200

    add Info modal

--- src/shared/components/navigation/navigation.component.tsx
+++ src/shared/components/navigation/navigation.component.tsx
@@ -3,11 +3,19 @@ import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
-import {GitHub as GitHubIcon, AddToPhotos} from '@material-ui/icons';
+import {
+  GitHub as GitHubIcon,
+  AddToPhotos,
+  // Share as ShareIcon,
+  InfoTwoTone,
+} from '@material-ui/icons';
+
 import LinkExternal from '@material-ui/core/Link';
 import {urls} from '../../externalURL';
 import {SearchBar} from '../filter/SearchBar/SearchBar.container.component';
 import {Link} from 'react-router-dom';
+import InfoModal from './../infoModal/detailModal.component';
+
 const Logo = require('./../../../assets/logos/CL_Logo.svg') as string;
 interface Props {
   adminCredentials?: string;
@@ -50,6 +58,11 @@ const useStyles = makeStyles((theme: Theme) =>
 
 export default function NavigationComponent(props: Props) {
   const classes = useStyles();
+  const [infoOpen, setInfoOpen] = React.useState(false);
+  const handleInfoOpen = () => {
+    setInfoOpen(!infoOpen);
+  };
+
   return (
     <>
       <AppBar position="fixed" className={classes.appBar}>
@@ -68,7 +81,13 @@ export default function NavigationComponent(props: Props) {
               </IconButton>
             </Link>
           )}
-
+          <IconButton onClick={handleInfoOpen} className={classes.button}>
+            <InfoTwoTone className={classes.button} />
+            <InfoModal open={infoOpen} handleClose={handleInfoOpen} />
+          </IconButton>
+          {/* for later IDEAS: <IconButton onClick={handleInfoOpen} className={classes.button}>
+            <ShareIcon className={classes.button} />
+          </IconButton> */}
           <LinkExternal
             href={urls.github}
             target="_blank"
