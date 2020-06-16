de24a87de9756e6dff8b0a122bfdcb25b65dcc64
Elias Keis <elias.keis@maibornwolff.de>
2020-06-16 12:05:15 +0200

    preview image

--- src/shared/components/serviceeditor/imageSelect.component.tsx
+++ src/shared/components/serviceeditor/imageSelect.component.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Grid, TextField, TextFieldProps} from '@material-ui/core';
+import {createStyles, makeStyles} from '@material-ui/core/styles';
 
 const PATH_PREFIX = './img/logos/';
 const PATH_PREFIX_REGEX = /^[.][/\\]img[/\\]logos[/\\](.*)$/;
@@ -10,7 +11,29 @@ export interface Props {
   imagePath: string;
 }
 
+const useStyles = makeStyles(() =>
+  createStyles({
+    notFound: {
+      color: 'red',
+    },
+    icon: {
+      height: 45,
+    },
+  })
+);
+
 export default function ImageSelect(props: Props) {
+  const classes = useStyles();
+  const [imgNotFound, setImgNotFound] = useState(false);
+
+  useEffect(() => {
+    setImgNotFound(false);
+  }, [props.imagePath]);
+
+  const handleInvalidPath = () => {
+    setImgNotFound(true);
+  };
+
   const handleImgPathChange = (event: React.ChangeEvent<HTMLInputElement>) =>
     props.onImagePathChanged(convertFromShownImgPath(event.target.value));
 
@@ -27,13 +50,29 @@ export default function ImageSelect(props: Props) {
   const shownImgPath = convertToShownImgPath(props.imagePath);
   return (
     <Grid container>
-      <Grid item xs={10}>
+      <Grid item xs={11}>
         <TextField
           value={shownImgPath}
           onChange={handleImgPathChange}
           fullWidth
           {...props.textFieldProps}
         />
+      </Grid>
+      <Grid item xs={1}>
+        {imgNotFound ? (
+          <span className={classes.notFound}>Not found!</span>
+        ) : (
+          <img
+            src={
+              props.imagePath.startsWith('.')
+                ? props.imagePath.substring(1)
+                : props.imagePath
+            }
+            alt="Service Icon"
+            onError={handleInvalidPath}
+            className={classes.icon}
+          />
+        )}
       </Grid>
     </Grid>
   );
