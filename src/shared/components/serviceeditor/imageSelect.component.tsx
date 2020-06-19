41b0dc6a485d0ee56bb425fa48125d5f9a3874e7
Elias Keis <elias.keis@maibornwolff.de>
2020-06-19 17:30:24 +0200

    use image icon in image select

--- src/shared/components/serviceeditor/imageSelect.component.tsx
+++ src/shared/components/serviceeditor/imageSelect.component.tsx
@@ -1,6 +1,6 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {Grid, TextField, TextFieldProps} from '@material-ui/core';
-import {createStyles, makeStyles} from '@material-ui/core/styles';
+import ServiceIcon from '../serviceIcon/serviceIcon.component';
 
 const PATH_PREFIX = './img/logos/';
 const PATH_PREFIX_REGEX = /^[.][/\\]img[/\\]logos[/\\](.*)$/;
@@ -11,29 +11,7 @@ export interface Props {
   imagePath: string;
 }
 
-const useStyles = makeStyles(() =>
-  createStyles({
-    notFound: {
-      color: 'red',
-    },
-    icon: {
-      height: 45,
-    },
-  })
-);
-
 export default function ImageSelect(props: Props) {
-  const classes = useStyles();
-  const [imgNotFound, setImgNotFound] = useState(false);
-
-  useEffect(() => {
-    setImgNotFound(false);
-  }, [props.imagePath]);
-
-  const handleInvalidPath = () => {
-    setImgNotFound(true);
-  };
-
   const handleImgPathChange = (event: React.ChangeEvent<HTMLInputElement>) =>
     props.onImagePathChanged(convertFromShownImgPath(event.target.value));
 
@@ -51,20 +29,15 @@ export default function ImageSelect(props: Props) {
   return (
     <Grid container>
       <Grid item xs={1}>
-        {imgNotFound ? (
-          <span className={classes.notFound}>Not found!</span>
-        ) : (
-          <img
-            src={
-              props.imagePath.startsWith('.')
-                ? props.imagePath.substring(1)
-                : props.imagePath
-            }
-            alt="Service Icon"
-            onError={handleInvalidPath}
-            className={classes.icon}
-          />
-        )}
+        <ServiceIcon
+          height={45}
+          src={
+            props.imagePath.startsWith('.')
+              ? props.imagePath.substring(1)
+              : props.imagePath
+          }
+          alt="Service Icon"
+        />
       </Grid>
       <Grid item xs={11}>
         <TextField
