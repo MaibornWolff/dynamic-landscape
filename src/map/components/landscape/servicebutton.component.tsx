a720785cb773cb9929df11b40b08641749a1a534
Elias Keis <elias.keis@maibornwolff.de>
2020-06-24 16:31:50 +0200

    highlight non-published services

--- src/map/components/landscape/servicebutton.component.tsx
+++ src/map/components/landscape/servicebutton.component.tsx
@@ -1,8 +1,9 @@
 import React, {useEffect, useState} from 'react';
 import {IconButton, Tooltip} from '@material-ui/core';
-import {DemoData} from '../../../assets/data/dataType';
+import {DemoData, ServiceStatus} from '../../../assets/data/dataType';
 import styled from 'styled-components';
 import ServiceIcon from '../../../shared/components/serviceIcon/serviceIcon.component';
+import {createStyles, makeStyles} from '@material-ui/core/styles';
 
 interface Props {
   service: DemoData;
@@ -12,9 +13,23 @@ interface Props {
   shouldScroll: boolean;
 }
 
+const backgroundForStatus = new Map<ServiceStatus, string>([
+  ['deleted', 'darkgrey'],
+  ['review', 'green'],
+]);
+
 const ServiceIconButton = styled(IconButton)({padding: 3});
 
+const useStyles = makeStyles(() =>
+  createStyles({
+    button: (props: {status: ServiceStatus}) => ({
+      backgroundColor: backgroundForStatus.get(props.status),
+    }),
+  })
+);
+
 function ServiceButton(props: Props) {
+  const classes = useStyles({status: props.service.status});
   const buttonRef = React.createRef<HTMLButtonElement>();
 
   const setDetailService = () => props.setDetailService(props.service);
@@ -34,6 +49,7 @@ function ServiceButton(props: Props) {
         aria-label={props.service.service}
         onClick={setDetailService}
         ref={buttonRef}
+        className={classes.button}
       >
         <ServiceIcon
           src={props.service.img}
